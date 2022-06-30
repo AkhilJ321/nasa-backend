5d84f79d57c68558fee12981401a53cfb40ce56c
AkhilJ321 <jamwalakhil936@gmail.com>
2022-06-30 17:06:42 +0530

    complleted with all the database

--- server/src/server.js
+++ server/src/server.js
@@ -1,27 +1,18 @@
 const http = require("http");
 const app = require("./app");
-const mongoose = require("mongoose");
-const { loadPlanetsData } = require("./models/planets.model");
 
-const MONGO_URL =
-  "mongodb+srv://AkhilJ321:Akhil123@cluster0.oo52r.mongodb.net/?retryWrites=true&w=majority";
+require("dotenv").config();
+const { mongoConnect } = require("./services/mongo");
+const { loadPlanetsData } = require("./models/planets.model");
+const { loadLaunchData } = require("./models/launches.model");
+
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready!");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
 }
 
 server.listen(PORT, () => {
